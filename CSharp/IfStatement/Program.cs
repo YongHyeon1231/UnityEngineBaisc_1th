a1952d604ff6e7cd6bd1e2254bfa636b1288e99f
YongHyeon1231 <120100530+YongHyeon1231@users.noreply.github.com>
2022-12-15 21:42:39 +0900

    C# - if, Operators
    1. If
    - Usage of if statement
    
    2. Operators
    - Took a look general c# operators

---
+++ CSharp/IfStatement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace IfStatement
+{
+    
+    internal class Program
+    {
+        static bool condition1 = false;
+        static bool condition2 = false;
+        static bool condition3 = false;
+
+        static void Main(string[] args)
+        {
+            /*if (조건1)
+            {
+                조건 1이 참일 때 실행할 내용
+            }
+            else if (조건 2)
+            {
+                조건 1이 거짓이고 조건2가 참일 때 실행할 내용
+            }
+            else
+            {
+                위 조건(들) 이 모두 거짓일 때 실행할 내용
+            }*/
+
+            if (condition1)
+            {
+                Console.WriteLine("조건 1이 참이다");
+            }
+            else if (condition2)
+            {
+                Console.WriteLine("조건 1이 거짓이고 조건 2가 참이다");
+            }
+            else
+            {
+                Console.WriteLine("조건 1,2 모두 거짓이다.");
+            }
+            
+
+        }
+    }
+}
